93c17c0a03396c285ab91b948ed6b705750be236
Kaushal Kumar <kaushaljee82@gmail.com>
2024-06-15 12:45:20 +0530

    Initialize matrix and normalize it

--- EigenGanme.cpp
+++ EigenGanme.cpp
@@ -16,6 +16,32 @@ void setValueOfn(){
 MatrixXf makeSymmetric(const MatrixXf& A) {
     return 0.5 * (A + A.transpose());
 }
+MatrixXf normalizeVector(const MatrixXf& vect) {
+    MatrixXf normalizedVector = vect;
+    float maxElement = vect.cwiseAbs().maxCoeff();
+    for (int i = 0; i < vect.size(); ++i) {
+        normalizedVector(i) /= maxElement;
+    }
+    return normalizedVector;
+}
+void insertColumnMatrix(MatrixXf& originalMatrix, const MatrixXf& columnMatrix) {
+    if (columnMatrix.rows() != originalMatrix.rows()) {
+        cerr << "Dimention of Column & Rows not match" <<endl;
+        return;
+    }
+    originalMatrix.conservativeResize(originalMatrix.rows(), originalMatrix.cols() + columnMatrix.cols());
+    originalMatrix.rightCols(columnMatrix.cols()) = columnMatrix;
+}
+void deleteColumn(MatrixXf& matrix, int columnIndex) {
+    if (columnIndex < 0 || columnIndex >= matrix.cols()) {
+        cerr << "Error: Column index out of bounds." << endl;
+        return;
+    }
+    for (int i = columnIndex; i < matrix.cols() - 1; ++i) {
+        matrix.col(i) = matrix.col(i + 1);
+    }
+    matrix.conservativeResize(matrix.rows(), matrix.cols() - 1);
+}
 int main() {
      setValueOfn();
     // Equation Ax = nBx
@@ -33,7 +59,20 @@ int main() {
     ges.compute(A, B);
     MatrixXf eigenvalues = ges.eigenvalues().real();
     MatrixXf eigenvectors = ges.eigenvectors().real();
+
     cout << "Eigenvalues: \n" << eigenvalues << endl;
     cout << "Eigenvectors: \n" << eigenvectors << endl;
+
+    
+    MatrixXf EigenVector(n,1);
+    MatrixXf temp;
+    for(int i=0; i<eigenvectors.cols(); i++){
+        temp = normalizeVector(eigenvectors.col(i));
+        insertColumnMatrix(EigenVector , temp);
+        deleteColumn(temp , 0);
+    }
+    deleteColumn(EigenVector , 0);
+    cout<<"Normalized Eigen Vector\n"<<EigenVector<<endl;
+
     return 0;
 }
