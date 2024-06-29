be9ac4fa654fb7be304965d9c67776c318d7b104
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2024-06-29 14:35:05 +0530

    Add return type function

--- HPP_FILE/EigenGame.cpp
+++ HPP_FILE/EigenGame.cpp
@@ -4,14 +4,26 @@
 
 using namespace Eigen;
 using namespace std;
-
+MatrixXf makeSymmetric(const MatrixXf& A) {
+    return 0.5 * (A + A.transpose());
+}
+MatrixXf eigenValues(const MatrixXf& eigenvectors) {
+    SelfAdjointEigenSolver<MatrixXf> solver(eigenvectors.transpose() * eigenvectors);
+    return solver.eigenvalues();
+}
 int main() {
-    int n = 5;
+    int n = 7;
     MatrixXf matrixA , matrixB;
     matrixA.setRandom(n,n);
     matrixA = matrixA.array().abs(); // Ensure positive values
     matrixB.setRandom(n,n);
     matrixB = matrixB.array().abs(); // Ensure positive values
-    solveEigenGame(matrixA, matrixB, n);
+    MatrixXf A = makeSymmetric(matrixA);
+    MatrixXf B = makeSymmetric(matrixB);
+    MatrixXf EigenVector = solveEigenGame(A , B , n);
+    MatrixXf EigenValues = eigenValues(EigenVector);
+    cout<<"Matrix A\n"<<A<<endl;
+    cout<<"Matrix B\n"<<B<<endl;
+    cout<<"Eigen Vector\n"<<EigenVector<<endl;
     return 0;
 }
